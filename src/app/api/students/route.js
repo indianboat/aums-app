726fb4982bb73609e0206fa0ae3ee506d4e3e809
pankaj <iampankaj852@gmail.com>
2023-09-05 11:38:44 +0530

    fixed prod

--- src/app/api/students/route.js
+++ src/app/api/students/route.js
@@ -2,12 +2,15 @@ import { NextResponse } from "next/server";
 import connectDB from "../../../../middleware/db";
 import Student from "../../../../models/student";
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 export async function GET(request){
   try {
     await connectDB();
     const studs = await Student.find({});
     return new NextResponse(JSON.stringify(studs), {status:200});
   } catch (error) {
-    return NextResponse.json({error:"500 Internal Server Error: " +error}, {status:500});
+    return NextResponse.json({error:"500 Internal Server Error: " + error}, {status:500});
   }
 }
