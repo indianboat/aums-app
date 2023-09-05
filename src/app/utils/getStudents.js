fab3a098a1982a287d8d1deb42e31758e0e22aa2
pankaj <iampankaj852@gmail.com>
2023-09-05 10:30:12 +0530

    fixed prod

--- src/app/utils/getStudents.js
+++ src/app/utils/getStudents.js
@@ -1,5 +1,5 @@
 export default async function getStudents() {
-  const res = await fetch("https://aums.vercel.app/api/students", { cache:"default" });
+  const res = await fetch("https://aums.vercel.app/api/students", { cache:"no-cache" });
 
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
